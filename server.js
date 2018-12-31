f94e640c264af286b6562b1d4c7847da8dff90f5
mlebjorke <mlestraus@gmail.com>
2018-12-30 21:14:31 -0700

    added link

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 // app.use(express.static(path.join(__dirname, '/app/public')));
 
-// require("./app/routing/apiRoutes")(app);
+require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
 
@@ -32,14 +32,8 @@ require("./app/routing/htmlRoutes")(app);
 // 1. Your survey should have 10 questions of your choosing. Each answer should be on a scale of 1 to 5 based on how much the user agrees or disagrees with a question.
 
 
-// 3. Your `htmlRoutes.js` file should include two routes:
-
-//    * A GET Route to `/survey` which should display the survey page.
-//    * A default, catch-all route that leads to `home.html` which displays the home page.
-
 // 4. Your `apiRoutes.js` file should contain two routes:
 
-//    * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
 //    * A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
 
 // 5. You should save your application's data inside of `app/data/friends.js` as an array of objects. Each of these objects should roughly follow the format below.
